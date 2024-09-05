f96c19e515e40c28a1f77c7a130179b5dd5620e3
DIRK TOMOTHY CARVAJAL <23010741@comlabserver.csabcomlab.edu.ph>
2024-09-05 17:52:25 +0800

    new code

--- index.php
+++ index.php
@@ -1,5 +1,41 @@
-<? php
+<?php 
 
-echo "Hello World!";
+class Payroll {
+
+	private string $firstname = "kurt";
+	private string $lastname = "allyson";
+
+	private float $tax = 80;
+	private float $salary = 500;
+
+
+
+	public function computeFinalGrades() {
+		$ave = $this->tax + $this->salary;
+		return $ave / 3;
+	}
+
+
+	public function getCompleteName() : string {
+		return $this->lastname." , " .$this->firstname;
+	}
+
+	public function displayGrade() : string {
+		return number_format($this->computeFinalGrades(),2);
+	}
+
+
+
+
+}
+
+
+
+$Payroll = new Payroll();
+
+echo "Student Name: ". $Payroll->getCompleteName() . "<br/r";
+echo "Final Grade: ".  $Payroll->computeFinalGrades();
+
+
 
 ?>
